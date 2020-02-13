6a9a22229032bd47cfdac6110d01d420c7c80a21
Roberto Fusco Pugliese <pugliesr@gmail.com>
2020-02-13 14:51:46 +0200

    Update Peristaltic_Arduino.ino

--- Arduino/Peristaltic_Arduino/Peristaltic_Arduino.ino
+++ Arduino/Peristaltic_Arduino/Peristaltic_Arduino.ino
@@ -4,12 +4,15 @@ void setup()
 {
   pinMode(motor, OUTPUT); //sets the digital pin as output
   digitalWrite(motor, HIGH); //turns the LED on
-  delay(100);
+  delay(3000);
   digitalWrite(motor, LOW);
   delay(3000);
 }
 
 void loop()
 {
-  
+//  digitalWrite(motor, HIGH); //turns the LED on
+//  delay(100);
+//  digitalWrite(motor, LOW);
+//  delay(3000);
 }
