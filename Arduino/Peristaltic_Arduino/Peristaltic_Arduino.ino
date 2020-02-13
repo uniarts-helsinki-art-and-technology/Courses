e8eea54cc6cdb1bb4c034b73e964b621a6e69d35
Roberto Fusco Pugliese <pugliesr@gmail.com>
2020-02-13 14:38:30 +0200

    Update Peristaltic_Arduino.ino

--- Arduino/Peristaltic_Arduino/Peristaltic_Arduino.ino
+++ Arduino/Peristaltic_Arduino/Peristaltic_Arduino.ino
@@ -3,13 +3,13 @@ const int motor = 10; //LED connected to digital pin 10
 void setup()
 {
   pinMode(motor, OUTPUT); //sets the digital pin as output
-  
-}
-
-void loop()
-{
   digitalWrite(motor, HIGH); //turns the LED on
   delay(100);
   digitalWrite(motor, LOW);
   delay(3000);
+}
+
+void loop()
+{
+  
 }
